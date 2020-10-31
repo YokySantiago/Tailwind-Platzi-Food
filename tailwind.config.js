e5a0045abdaaf97d68065c7da62e0779e45c1e7c
Yoky Code <yoky@code.com>
2020-10-31 16:05:11 -0500

    Se ajustan algunos detalles de la vista principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -730,7 +730,11 @@ module.exports = {
     extend: {
       colors: {
         primary: '#f3c614',
-        secundary:  '#353535'
+        secundary:  '#31393c',
+        gold: '#928028',
+        error: '#D36582',
+        success: '#00A878',
+        info: '#4D6CFA'
       },
       height: {
         80: '20rem',
