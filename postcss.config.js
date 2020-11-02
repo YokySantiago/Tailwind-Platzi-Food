2711df193c851f3b6212628d1acbb19d68c2d004
Yoky Code <yoky@code.com>
2020-11-02 11:15:04 -0500

    Se agrega componentes, se agrega libreria purgecss y nanocss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
         require('postcss-import'),
         require('postcss-nested'),
         require('tailwindcss'),
         require('autoprefixer'),
+        purgecss({
+            content: ['./**/*.html'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        }),
+        require('cssnano')({
+            preset: 'default',
+        }),
     ]
 }
