971f70db67f4a29ec486eb96c0cc910a785b0317
Yoky Code <yoky@code.com>
2021-01-07 18:56:33 -0500

    Tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('postcss-nested'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-        purgecss({
-            content: ['./**/*.html'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        }),
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ]
-}
+  plugins: [
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss({
+      content: ["./**/*.html"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
